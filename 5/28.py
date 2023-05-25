e663fec79fa53b998d6ea49a7fa7c96afcc27694
Ershov Dmitrii <dima52052@mail.ru>
2023-05-25 13:16:56 +0500

    s7/34-36

--- 5/28.py
+++ 5/28.py
@@ -1,9 +1,11 @@
 a = int(input('Введите A: '))
 b = int(input('Введите B: '))
 
+
 def sum(a, b):
     if a == 0:
         return b;
     return sum(a - 1, b + 1)
 
-print(sum(a,b))
+
+print(sum(a, b))
---
+++ Home/list.py
@@ -0,0 +1,24 @@
+# Пустой список
+list_1 = []
+print(list_1)
+
+# Пустой список
+list_2 = list()
+print(list_2)
+
+# Список 1,2,3
+list_3 = [1, 2, 3]
+print(list_3)
+
+# Длина списка
+print(len(list_3))
+
+# Ввывод 0 симвала из списка
+print(list_3[0])
+
+
+# put your python code here
+a = int(input())
+b = int(input())
+f = a - b
+print(f)
